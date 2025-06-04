8f0d1e7010e9fd3b0f533408f4b9211bf6e1c602
Karl-Philipp Flösch <karl-philipp.floesch@uni-konstanz.de>
2025-06-04 13:53:15 +0200

    Update niceTFRplot.m
    added colorspace package

--- niceTFRplot.m
+++ niceTFRplot.m
@@ -13,6 +13,7 @@
 % Requires:
 % - FieldTrip toolbox, freely available under https://www.fieldtriptoolbox.org/download/
 % - cbrewer2, freely available under https://de.mathworks.com/matlabcentral/fileexchange/58350-cbrewer2
+% - Colorspace Transformations, freely available under https://www.mathworks.com/matlabcentral/fileexchange/28790-colorspace-transformations
 % - sensor net layout of your EEG net (e.g., .sfp-file); must be compatible with FieldTrip
 % - output struct of cluster statistic from ft_freqstatistics in a .mat file
 %
@@ -21,6 +22,7 @@
 % section below.
 % 
 % References
+% Getreuer, P. (2025). Colorspace Transformations (https://www.mathworks.com/matlabcentral/fileexchange/28790-colorspace-transformations), MATLAB Central File Exchange.
 % Lowe, S. (2025). cbrewer2 (https://github.com/scottclowe/cbrewer2), GitHub.
 % Oostenveld, R., Fries, P., Maris, E., Schoffelen, J.-M. (2011). FieldTrip: Open source software for advanced analysis of MEG, EEG, and invasive electrophysiological data. Computational Intelligence and Neuroscience, 2011, 156869, doi:10.1155/2011/156869.
 %
@@ -38,6 +40,9 @@ opts.ftPath       = 'XXXXXX';
 % FOLDER PATH TO cbrewer2
 opts.cbrewer2Path = 'XXXXXX';
 
+% FOLDER PATH TO Colorspace Transformations
+opts.colspacePath = 'XXXXXX';
+
 % FILE PATH TO NET LAYOUT (must be compatible with FieldTrip)
 opts.netLayout    = 'XXXXXX';
 
@@ -45,6 +50,7 @@ opts.netLayout    = 'XXXXXX';
 opts.dataPath     = 'XXXXXX';
 
 addpath(opts.cbrewer2Path)
+addpath(opts.colspacePath)
 addpath(opts.ftPath)
 ft_defaults
 
